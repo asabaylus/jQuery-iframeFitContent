9d5594f69786b2ec01f5fb8e7c0ef6754e0ec9eb
Asa <asa@baylus.com>
2011-02-03 23:25:20 -0500

    adding some tests

--- src/jquery.iframefitcontent.js
+++ src/jquery.iframefitcontent.js
@@ -29,152 +29,191 @@
 	$.fn.extend({
 		iframeFitContent: function (options, method) {			
 				
-					
-    				
+
 					var defaults = {
 						resize: 'both' // sets which dimensions to resize: height, width or both
 						},
+						_version = "1.0.1",
+						_origHeight,
+						_origWidth,
 						$iframe = $(this), 
-						opt = options,
+						$iframeContent, 
+						$iframeWrapper,
+						opt = options  = {},
 						now = new Date(),
 						methods = {
 							init: function(){
 								
-								//console.log("init!");
-								
-								
+								console.log("init!");
+									
+									
 								return this.each(function () {
-								
-								
-					            var $iframe = $(this), 
-									opt = options;
 									
-									//console.log($iframe, opt);
-									
-									$iframe.one("load" , function(){
-																		
-											// clear the browser cache by makeing the iframe url unique
-											$iframe.attr("scrolling" , "no").attr({
-												"src" : this.src + "?z=" +  now.getTime().toString()
-											}).bind("load", function(){
-												
-												//$.fn.iframeFitContent(opt,'render');
-												return methods.render.apply( $iframe );
-											});
+										$(document).trigger("iframeFitContentReady", [now.getTime(), _version]);
 										
-									});
-								});			
-								
-							},
-							render : function() {
-								
-								
-								//console.log('render!');
-								
-								
-								
-								// hide the iframe until it loaded to avoid the height pop
-								$iframe.css({visibility: "hidden"});		
-						
-								// create vars
-								var $iframeContent, 
-									$iframeWrapper
-
-								// get iframe contents
-								$iframeContent = $iframe.contents().find("body");
-								
-								// console.log($iframe.contents());
-								
-								
-								// wrap the page content in a div with a unique ID
-								$iframeContent.children().wrapAll("<div id='iframeWrapper" + now.getTime().toString() + "'></div>");
-								
-								// cache the wrapper
-								$iframeWrapper = $iframeContent.find("#iframeWrapper" + now.getTime().toString());
-								
-								// copy the margins and borders from the body onto the wrapper div
-								// convert margins to padding for IE6 or we end up with too much height
-								$iframeWrapper.css({
-									'padding-top' : $iframeContent.css("margin-top"),
-									'padding-bottom' : $iframeContent.css("margin-bottom"),	
-									'padding-left' : $iframeContent.css("margin-left"),
-									'padding-right' : $iframeContent.css("margin-right"),
-									'border-left-width' : $iframeContent.css("border-left-width"),
-									'border-right-width' : $iframeContent.css("border-right-width"),
-									'border-top-width' : $iframeContent.css("border-top-width"),
-									'border-bottom-width' : $iframeContent.css("border-bottom-width"),
-									'border-left-style' : $iframeContent.css("border-left-style"),
-									'border-right-style' : $iframeContent.css("border-right-style"),
-									'border-top-style' : $iframeContent.css("border-top-style"),
-									'border-bottom-style' : $iframeContent.css("border-bottom-style"),
-									'border-left-color' : $iframeContent.css("border-left-color"),
-									'border-right-color' : $iframeContent.css("border-right-color"),
-									'border-top-color' : $iframeContent.css("border-top-color"),
-									'border-bottom-color' : $iframeContent.css("border-bottom-color"),
-									'position' : 'absolute',
-									'height' : 'auto',
-									'width' : 'auto'				  
-								});
-								
-								// remove margins from the iframe > body 
-								$iframeContent.css({
-									'border' : 'none',
-									'margin-top' : '0',
-									'margin-bottom' : '0',
-									'margin-left' : '0',
-									'margin-right' : '0'
-								});
-								
-								// if resize is set then apply new size to the correct dimensions then
-								// set height and width to wrapper + margins top & bot + border height + extra padding												
-								if (opt.resize !== 'width') {
-									$iframe.height($iframeWrapper.outerHeight(true));
-								}
-								
-								if (opt.resize !== 'height') {
-									$iframe.width($iframeWrapper.outerWidth(true));
-								}
-								
-								
-								// set the wrapper back to relative or we'll lose wrapping
-								// add zoom to fix IE6 has layout bug
-								$iframeWrapper.css({
-									'position' : 'relative',
-									'zoom' : '1'	  
-								});  
-								
-								// show the iframe when finished
-								$iframe.css("visibility", "visible");	
-								
-								
-								// make plugin chainable
-								// beware the height / width will be wrong in the chain
-								return this;
-								
-								
-							}
+							            var $iframe = $(this), 
+											opt = options;
+											
+											// get the originl dimensions
+											// we'll use them during destroy
+											_origHeight = $iframe.height();
+											_origWidth = $iframe.width();
+											
+											
+											
+											//console.log($iframe, opt);
+											
+											$iframe.one("load" , function(){
+																				
+													// clear the browser cache by makeing the iframe url unique
+													$iframe.attr("scrolling" , "no").attr({
+														"src" : this.src + "?z=" +  now.getTime().toString()
+													}).bind("load", function(){
+														
+														//$.fn.iframeFitContent(opt,'render');
+														return methods.render.apply( $iframe );
+													});
+												
+											});
+										});			
+									
+								},
+								render : function() {
+									
+									
+									console.log('render!');
+									
+									
+									
+									// hide the iframe until it loaded to avoid the height pop
+									$iframe.css({visibility: "hidden"});		
 							
-						};
-
-
-					
-					options = $.extend(defaults, options);
-					
-					
-					if ( methods[method] ) {
-						// need to better understand  Array.prototype.slice.call( arguments, 1 ) I dont see the point
-						return methods[ method ].apply(this, Array.prototype.slice.call( arguments, 1 ));
-					} else if ( typeof method === 'object' || ! method ) {
-						return methods.init.apply( $(this), arguments );
-					} else {
-						$.error( 'Method ' +  method + ' does not exist on jQuery.iframeFitContent' );
-					}    
-
-						
-					
-
-				}
 	
+									// get iframe contents
+									$iframeContent = $iframe.contents().find("body");
+									
+									// console.log($iframe.contents());
+									
+									
+									// wrap the page content in a div with a unique ID
+									$iframeContent.children().wrapAll("<div id='iframeWrapper" + now.getTime().toString() + "'></div>");
+									
+									// cache the wrapper
+									$iframeWrapper = $iframeContent.find("#iframeWrapper" + now.getTime().toString());
+									
+									console.log($iframeWrapper);
+									
+									// copy the margins and borders from the body onto the wrapper div
+									// convert margins to padding for IE6 or we end up with too much height
+									$iframeWrapper.css({
+										'padding-top' : $iframeContent.css("margin-top"),
+										'padding-bottom' : $iframeContent.css("margin-bottom"),	
+										'padding-left' : $iframeContent.css("margin-left"),
+										'padding-right' : $iframeContent.css("margin-right"),
+										'border-left-width' : $iframeContent.css("border-left-width"),
+										'border-right-width' : $iframeContent.css("border-right-width"),
+										'border-top-width' : $iframeContent.css("border-top-width"),
+										'border-bottom-width' : $iframeContent.css("border-bottom-width"),
+										'border-left-style' : $iframeContent.css("border-left-style"),
+										'border-right-style' : $iframeContent.css("border-right-style"),
+										'border-top-style' : $iframeContent.css("border-top-style"),
+										'border-bottom-style' : $iframeContent.css("border-bottom-style"),
+										'border-left-color' : $iframeContent.css("border-left-color"),
+										'border-right-color' : $iframeContent.css("border-right-color"),
+										'border-top-color' : $iframeContent.css("border-top-color"),
+										'border-bottom-color' : $iframeContent.css("border-bottom-color"),
+										'position' : 'absolute',
+										'height' : 'auto',
+										'width' : 'auto'				  
+									});
+									
+									// remove margins from the iframe > body 
+									$iframeContent.css({
+										'border' : 'none',
+										'margin-top' : '0',
+										'margin-bottom' : '0',
+										'margin-left' : '0',
+										'margin-right' : '0'
+									});
+									
+									// if resize is set then apply new size to the correct dimensions then
+									// set height and width to wrapper + margins top & bot + border height + extra padding												
+									if (opt.resize !== 'width') {
+										$iframe.height($iframeWrapper.outerHeight(true));
+									}
+									
+									if (opt.resize !== 'height') {
+										$iframe.width($iframeWrapper.outerWidth(true));
+									}
+									
+									
+									// set the wrapper back to relative or we'll lose wrapping
+									// add zoom to fix IE6 has layout bug
+									$iframeWrapper.css({
+										'position' : 'relative',
+										'zoom' : '1'	  
+									});  
+									
+									// show the iframe when finished
+									$iframe.css("visibility", "visible");	
+									
+									
+									// make plugin chainable
+									// beware the height / width will be wrong in the chain
+									return this;
+									
+									
+								},
+								
+								originalWidth : function (){
+									return this.each(function(){
+										console.log("original width", _originalWidht);
+										return _origWidth;									
+									});
+									
+								},
+								
+								originalHeight : function (){
+									return _origHeight;
+								},
+								destroy : function(){
+									console.log("destroy");	
+								}
+								
+							};
+	
+	
+						
+						options = $.extend(defaults, options);
+
+	/*
+					
+						if (!opt) {
+						
+							method = opt;
+						}
+*/
+
+						if (!options) { options = method};
+					    console.log("options",options);
+					    console.log("method",method);
+						console.log("methods[method]", methods[method]);
+													
+						if ( methods[method] ) {
+							// need to better understand  Array.prototype.slice.call( arguments, 1 ) I dont see the point
+							console.log("run method = " + methods[ method ].toString());
+							return methods[ method ].apply(this, Array.prototype.slice.call( arguments, 1 ));
+						} else if ( typeof method === 'object' || ! method ) {
+							return methods.init.apply( $(this), arguments );
+						} else {
+							$.error( 'Method ' +  methods[method] + ' does not exist on jQuery.iframeFitContent' );
+						}    
+	
+							
+						
+	
+					}
+		
 		
 	});
 	
--- test/test.js
+++ test/test.js
@@ -1,2 +1,131 @@
 $(document).ready(function(){  
+  
+  // get the original dimensions
+  var origHeight = $("#testiframe").height(); 
+  var origWidth = $("#testiframe").width();
+  var pluginReady = [];
+  
+  // listen for the plugins ready event
+  $(document).bind("iframeFitContentReady", function(event, timeStamp, _version){
+   	pluginReady = [timeStamp, _version];
+  });	
+
+	
+  	
+
+  // these tests verfiy that the dimensions of the test iframe
+  // are different from the iframe's original dimensions
+  // I dont need to worry thet the dimensions will match during these test
+  // since I know the sample content getting loaded into the iframe
+  // does not match the existing dimensions.
+	
+ module("iframeFitContent initialization", {
+ 
+ 	setup: function(){
+		 		
+		  // init the plugin
+		  $("#testiframe").iframeFitContent();
+
+ 	}
+ 
+ });
+ 
+ test("test for plugin version", function(){
+ 		
+ 	// check that _version is defined
+	ok(pluginReady[1], "version: jQuery iframeFitContent " + pluginReady[1] );
+ 
+ });
+ 
+  test("test for plugin timeStamp", function(){
+  
+
+
+  		var re = new RegExp(pluginReady[0] + "$");
+		var iframeSrcTimeStamp = $("#testiframe").attr("src").match( re );
+		ok(pluginReady[0].toString(), "plugin timeStamp exists")
+		
+		// check that the plugin timestamp matches the test element
+		equals(pluginReady[0].toString(), iframeSrcTimeStamp, 'plugin timeStamp matches iframe src attribute "z" querystring parameter, prevents cached iframe content');
+ 
+ });
+
+
+test("test for iframe div wrapper exists", function(){
+
+		var $iframeWrapper = $("#testiframe").contents().find("body > #iframeWrapper"+pluginReady[0]);
+		equals($iframeWrapper.length, 1, "div > #iframeWrapper"+pluginReady[0].toString()+" added to iframe DOM");
+		
+});
+
+test("simulate a change to the iframe contents DOM dimensions" , function(){
+ 		
+ 	    // get the original dimensions
+	    var renderedHeight = $("#testiframe").height(); 
+	    var renderedWidth = $("#testiframe").width();
+	    
+	    var $iframeWrapper = $("#testiframe").contents().find("body > #iframeWrapper"+pluginReady[0]);
+	    
+		// simulate a change to the iframe content dimensions.
+		$iframeWrapper.append('<div style="display:block; width:1000px; height:1000px"></div>');
+		
+		// test the resize method
+		$("#testiframe").iframeFitContent({resize:'both'},'render');
+		
+		
+		notEqual($("#testiframe").height(), renderedHeight, "new iframe content height is not equal to old iframe content height");
+		notEqual($("#testiframe").width(), renderedWidth, "new iframe content width is not equal to old iframe content width");
+ 		
+ 		
+		
+
+ });
+
+
+ module("iframeFitContent Test Dimensions", {
+ 
+ 	setup: function(){
+		 		
+		  // init the plugin
+		  $("#testiframe").iframeFitContent();
+
+ 	},
+ 	teardown: function(){
+		 		
+		  // init the plugin
+		  $("#testiframe").iframeFitContent('destroy');
+
+ 	}
+ 
+ });
+ 
+ test("resize by height", function(){
+  		$("#testiframe").iframeFitContent({resize:'height'},'render');
+  		notEqual($("#testiframe").height(), origHeight, "original iframe height is not equal to new iframe height after content is resized to fit");
+ });
+ 
+  test("resize by width", function(){
+  		$("#testiframe").iframeFitContent({resize:'width'},'render');
+  		notEqual($("#testiframe").width(), origWidth, "original iframe width is not equal to new iframe width after content is resized to fit");
+ });
+ 
+   test("resize both height and width", function(){
+  		$("#testiframe").iframeFitContent({resize:'both'},'render');
+  		notEqual($("#testiframe").width(), origWidth, "original iframe width is not equal to new iframe width after content is resized to fit");
+  		notEqual($("#testiframe").height(), origHeight, "original iframe height is not equal to new iframe height after content is resized to fit");
+ });
+ 
+
+	
+
+/*
+  test("Sample test", function()  
+  {  
+     expect(1);  
+     equals(divide(4,2),  
+      2,  
+      'Expected 2 as the result, result was: ' + divide(4,2));  
+  });  
+*/
+
 });  
